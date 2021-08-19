e45b8aa5b508625f93013c12d02ea05865598142
Alice laptop <53339016+913AliceHincu@users.noreply.github.com>
2021-08-19 23:54:39 +0300

    test

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const App = () => {
           <>
             {showAddTask && <AddTask onAdd={addTask}/>}
             {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks to show!')}
-            {//test for git
+            {//testt for git
             }
           </>
         )}/>
