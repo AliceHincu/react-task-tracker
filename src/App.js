d16d29bafc3e5d104ba917c6c14880e421967a8c
Alice laptop <53339016+913AliceHincu@users.noreply.github.com>
2021-08-19 23:48:49 +0300

    test

--- src/App.js
+++ src/App.js
@@ -93,7 +93,8 @@ const App = () => {
           <>
             {showAddTask && <AddTask onAdd={addTask}/>}
             {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks to show!')}
-        
+            {//test for git
+            }
           </>
         )}/>
         <Route path='/about' component={About}/>
